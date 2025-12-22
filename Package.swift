f51ff1112f3d53adcaf854647271ac1f5b6ac810
Ven <ven.wu@baifu-tech.net>
2025-12-22 13:12:16 +0800

    [Feature] 升級 APKSignKey

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/coollazy/Image.git", from: "1.2.1"),
         .package(url: "https://github.com/jpsim/Yams.git", .upToNextMinor(from: "5.1.2")),
-        .package(url: "https://github.com/coollazy/APKSignKey.git", from: "1.0.0"),
+        .package(url: "https://github.com/coollazy/APKSignKey.git", from: "1.1.0"),
     ],
     targets: [
         .target(
